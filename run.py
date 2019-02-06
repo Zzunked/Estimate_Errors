e9abcb858789eaea9e8e6f2a3f9f882caf237caa
Zzunked <deadsunr1selp@gmail.com>
2019-02-06 21:41:25 +0300

    Initial commit

---
+++ run.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5 import QtWidgets
+from estimate_errors.indirerror import IndirErrorGUI
+
+def err_main_exec():
+    app = QtWidgets.QApplication(sys.argv)
+    win = IndirErrorGUI()
+    win.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    err_main_exec()
